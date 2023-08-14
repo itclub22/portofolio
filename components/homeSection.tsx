769f16b1e166419204309f85d4b25690b28374db
rulanugrh <nextarituenak01@gmail.com>
2023-08-14 12:14:46 +0700

    feat:profile section

--- components/homeSection.tsx
+++ components/homeSection.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import React from "react"
 import { HiArrowNarrowRight, HiChevronDoubleRight } from "react-icons/hi"
-import { FaGithub, FaYoutube, FaInstagram } from 'react-icons/fa'
 import AboutWe from "@/lib/aboutWe"
 import TypedAnim from "@/lib/forTyped"
 
@@ -9,11 +8,11 @@ export default function HomeSection() {
     return (
         <div className="lg:px-20 2xl:container px-6">
             <div className="bg-bgColor rounded-2xl backdrop-filter backdrop-blur-lg bg-opacity-40 pt-8 pb-8 shadow-lg shadow-black/50">
-                <div className="flex flex-wrap pb-8">
+                <div className="flex flex-wrap pb-10">
                     <div className="px-6">
                         <a className="text-blue-300 lg:text-xl text-md font-firaCode flex flex-wrap space-x-3 ">
                             <h1 className="pt-1 text-teal-500"><HiArrowNarrowRight /></h1>
-                            <h1 className="font-firaCode font-semibold lg:text-xl text-md"><span className="text-blue-300 inline">ceres </span><span className="inline text-blue-800">git:(</span><span className="inline text-red-600">master</span><span className="text-blue-800">)</span> $ <span className="inline-block text-white">./welcome.sh</span></h1>
+                            <h1 className="font-firaCode font-semibold lg:text-xl text-md"><span className="text-blue-300 inline">home </span><span className="inline text-blue-800">git:(</span><span className="inline text-red-600">master</span><span className="text-blue-800">)</span> $ <span className="inline-block text-white">./welcome.sh</span></h1>
                         </a>
                         
                     </div>
@@ -21,23 +20,23 @@ export default function HomeSection() {
                         <Image src={"/assets/bumi.webp"} width={400} height={400} alt="Maskot ITC" className=""/>
                     </div>
                     <div className="w-full text-center px-6">
-                        <h1 className="font-rubikFont lg:text-4xl font-bold text-shadowBox text-lg">Welcome to website IT Club</h1>
-                        <h3 className="font-firaCode text-sm lg:text-2xl italic text-gray-400 lg:mb-10 mb-5">IT クラブのウェブサイトへようこそ</h3>
+                        <h1 className="font-rubikFont lg:text-4xl font-bold text-shadowBox text-lg">Welcome to website IT Club 22</h1>
+                        <h3 className="font-firaCode text-sm lg:text-2xl italic text-gray-400 lg:mb-10 mb-5">Selamat Datang di Website IT Club 22</h3>
                     </div>
                 </div>
 
                 <div className="px-6">
                     <a className="text-blue-300 lg:text-xl text-md font-firaCode flex flex-wrap space-x-3 ">
                         <h1 className="pt-1 text-teal-500"><HiArrowNarrowRight /></h1>
-                        <h1 className="font-firaCode font-semibold lg:text-xl text-md"><span className="text-blue-300 inline">ceres </span><span className="inline text-blue-800">git:(</span><span className="inline text-red-600">master</span><span className="text-blue-800">)</span> $ <span className="inline-block text-white">./files about-we.txt</span></h1>
+                        <h1 className="font-firaCode font-semibold lg:text-xl text-md"><span className="text-blue-300 inline">home </span><span className="inline text-blue-800">git:(</span><span className="inline text-red-600">master</span><span className="text-blue-800">)</span> $ <span className="inline-block text-white">./about-we</span></h1>
                     </a>    
                 </div>
-                <div className="flex flex-wrap">
-                    <div className="w-full lg:w-1/2 lg:text-lg text-sm">
+                <div className="flex flex-wrap ">
+                    <div className="w-full lg:w-1/2 lg:text-lg text-sm lg:px-6">
                         <AboutWe />
                     </div>
                     <div className="w-full lg:w-1/2 pt-5 px-6">
-                        <h1 className="font-firaCode lg:text-3xl font-bold text-shadowBox text-lg">Hello 👋, we are itclub</h1>
+                        <h1 className="font-firaCode lg:text-3xl font-bold text-shadowBox text-lg">Hello 👋, we are itclub 22</h1>
                         <a className="font-firaCode text-sm lg:text-2xl text-gray-400 lg:mb-10 mb-5 flex flex-wrap space-x-3">
                             <h1 className="pt-1"><HiChevronDoubleRight /></h1>
                             <TypedAnim />
@@ -45,20 +44,20 @@ export default function HomeSection() {
                         <p className="font-firaCode text-sm lg:text-lg font-thin text-gray-300 lg:w-11/12 mb-10">Ekstrakurikuler ini bertujuan untuk mengasah bakat dan minat siswa didalam bidang Infromatika dan teknologi seperti mempelajari bahasa pemograman, membuat website, E-Sport, membahas seputar teknologi dan komputer. Ekskul ini juga biasa dilakukan untuk persiapan olimpiade komputer nasional atau lomba-lomba seputar teknologi dan informatika.</p>
                         <div className="grid grid-cols-8 lg:w-8/12">
                             <a className={`bg-black py-5 px-5`}></a>
-                            <a className={`bg-red-500 py-5 px-5`}></a>
-                            <a className={`bg-teal-500 py-5 px-5`}></a>
-                            <a className={`bg-yellow-500 py-5 px-5`}></a>
-                            <a className={`bg-blue-500 py-5 px-5`}></a>
-                            <a className={`bg-purple-500 py-5 px-5`}></a>
-                            <a className={`bg-blue-500 py-5 px-5`}></a>
+                            <a className={`bg-red-400 py-5 px-5`}></a>
+                            <a className={`bg-teal-400 py-5 px-5`}></a>
+                            <a className={`bg-yellow-400 py-5 px-5`}></a>
+                            <a className={`bg-blue-400 py-5 px-5`}></a>
+                            <a className={`bg-purple-400 py-5 px-5`}></a>
+                            <a className={`bg-blue-400 py-5 px-5`}></a>
                             <a className={`bg-white py-5 px-5`}></a>
-                            <a className={`bg-gray-500 py-5 px-5`}></a>
-                            <a className={`bg-red-500 py-5 px-5`}></a>
-                            <a className={`bg-teal-500 py-5 px-5`}></a>
-                            <a className={`bg-yellow-500 py-5 px-5`}></a>
-                            <a className={`bg-blue-500 py-5 px-5`}></a>
-                            <a className={`bg-purple-500 py-5 px-5`}></a>
-                            <a className={`bg-blue-500 py-5 px-5`}></a>
+                            <a className={`bg-gray-400 py-5 px-5`}></a>
+                            <a className={`bg-red-400 py-5 px-5`}></a>
+                            <a className={`bg-teal-400 py-5 px-5`}></a>
+                            <a className={`bg-yellow-400 py-5 px-5`}></a>
+                            <a className={`bg-blue-400 py-5 px-5`}></a>
+                            <a className={`bg-purple-400 py-5 px-5`}></a>
+                            <a className={`bg-blue-400 py-5 px-5`}></a>
                             <a className={`bg-white py-5 px-5`}></a>
                         </div>
                     </div>
---
+++ components/profileSection.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { HiArrowNarrowRight, HiChevronDoubleRight } from "react-icons/hi"
+import CardProfile from "@/lib/forCard";
+
+export default function ProfileSection() {
+    return (
+        <div className="lg:px-20 2xl:container px-6">
+            <div className="bg-bgColor rounded-2xl backdrop-filter backdrop-blur-lg bg-opacity-40 pt-8 pb-8 shadow-lg shadow-black/50">
+                <div className="flex flex-wrap pb-8">
+                    <div className="px-6">
+                        <a className="text-blue-300 lg:text-xl text-md font-firaCode flex flex-wrap space-x-3 ">
+                            <h1 className="pt-1 text-teal-500"><HiArrowNarrowRight /></h1>
+                            <h1 className="font-firaCode font-semibold lg:text-xl text-md"><span className="text-blue-300 inline">manager </span><span className="inline text-blue-800">git:(</span><span className="inline text-red-600">master</span><span className="text-blue-800">)</span> $ <span className="inline-block text-white">./print.o</span></h1>
+                        </a>
+                    </div>
+                    <div className="px-6 grid grid-cols-1 gap-4 lg:grid-cols-3 pt-5">
+                        
+                        <CardProfile Name="Gilang Putra" Uname="@glngputra" Desc="Leader of IT Club and Networking Enginner. Heyy, welcome to itclub 👋" Color="danger" Image="https://media.discordapp.net/attachments/1125397316971528272/1140492754703614042/7ac1b4ed-125c-4e4b-9fd1-d9174b8e1808.png?width=255&height=453" Jobs="#TinjuDibalasTinju" LinkIG="https://instagram.com" TTL="03 Feb 2005"/>
+                        <CardProfile Name="Fawaaz Nurul Aziz" Uname="@fwaziz" Desc="Vice Chairman of IT Club and MOBA Player. Heyy, welcome to itclub 👋" Color="primary" Image="https://media.discordapp.net/attachments/1125397316971528272/1140495682277478440/343189515_2417407311767272_967925466495578414_n.png?width=454&height=454" Jobs="#BusetKataGuaMah" LinkIG="https://instagram.com" TTL="31 Jun 2006"/>
+                        <CardProfile Name="Andri Ghifari" Uname="@andrigfry" Desc="Treasurer of IT Club and Cloud Enthusiast. Heyy, welcome to itclub 👋" Color="warning" Image="https://media.discordapp.net/attachments/1125397316971528272/1140493758085988372/ec24ec0c-7c47-4844-8540-25b3abd65caf.png?width=255&height=454" Jobs="#Hah?" LinkIG="https://instagram.com" TTL="06 Jun 2006"/>
+                        <CardProfile Name="Dina Amelia" Uname="@dinamelia" Desc="Secretary of IT Club and Brand Ambasador. Heyy, welcome to itclub 👋" Color="secondary" Image="https://media.discordapp.net/attachments/1125397316971528272/1140493995190001664/364415798_277398801578900_808243873745490818_n.png?width=454&height=454" Jobs="#XoXoo" LinkIG="https://instagram.com" TTL="02 Mar 2007"/>
+                        <CardProfile Name="Sukarno Adi" Uname="@kanon" Desc="Secretary of IT Club and MOBA Player. Heyy, welcome to itclub 👋" Color="primary" Image="https://media.discordapp.net/attachments/1125397316971528272/1140482923833864272/527bb113-4661-4aa8-92a2-572bb22fc68e.png?width=342&height=454" Jobs="#ApaCobaBang" LinkIG="https://instagram.com" TTL="22 Nov 2005"/>
+                        <CardProfile Name="Shivan Putra" Uname="@vann" Desc="Treasurer of IT Club and MVP IT Club. Heyy, welcome to itclub 👋" Color="warning" Image="https://media.discordapp.net/attachments/1125397316971528272/1140500000900657233/9a4b29c5-0af3-4efa-ac5b-f44a3c6604f0.png?width=342&height=454" Jobs="#BangTarikinGak?" LinkIG="https://instagram.com" TTL="30 Jan 2007"/>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
